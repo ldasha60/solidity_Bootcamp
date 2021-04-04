d7e56746d30864b997da5e5cfe8e595743e25539
ldasha60 <ldasha60@gmail.com>
2021-04-04 18:41:53 +0700

    update the withdrawal money function

--- MappingStructExample.sol
+++ MappingStructExample.sol
@@ -32,15 +32,15 @@ contract MappingsStructExample {
     }
     
     function withdrawAllMoney(address payable _to) public payable {
-        uint balanceToSend = balanceReceived[msg.sender];
-        balanceReceived[msg.sender] = 0;
+        uint balanceToSend = balanceReceived[msg.sender].totalBalance;
+        balanceReceived[msg.sender].totalBalance = 0;
         
         _to.transfer(balanceToSend);
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
-        require(_amount <= balanceReceived[msg.sender], "not enough funds");
-        balanceReceived[msg.sender] -= _amount;
+        require(_amount <= balanceReceived[msg.sender].totalBalance, "not enough funds");
+        balanceReceived[msg.sender].totalBalance -= _amount;
         _to.transfer(_amount);
         
     }
